314b2ee90712d4923b7c012792fe525e0f31e960
Abner Dascalakis <abnerdascalakis@gmail.com>
2026-07-29 13:34:37 -0400

    pastas parcialmente estruturadas

---
+++ app/checkout/page.tsx
@@ -0,0 +1,8 @@
+export function Checkout() {
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <p>Esta é a página de checkout.</p>
+    </div>
+  );
+}
---
+++ app/produtos/page.tsx
@@ -0,0 +1,7 @@
+export default function produtos() {
+  return (
+   <><div>
+    <h1>pagina produtos</h1>
+    </div></>
+  );
+}
